96187ea1d4f91108eccb30d75cbc3af00132576f
Ahumaraeze Princewill <princewillahumaraeze@gmail.com>
2025-05-31 14:17:25 +0100

    created the struct for the ICMP header

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,18 @@ use std::os::unix::io::AsRawFd;
 use std::time::{Duration};
 use std::net::UdpSocket;
 
+const ICMP_ECHO_REQUEST: u8 = 8;
+const ICMP_ECHO_REPLY: u8 = 0;
+
+#[repr(packed)]
+struct IcmpHeader{
+    icmp_type: u8,
+    icmp_code: u8,
+    checksum: u16,
+    identifier: u16,
+    sequence_header: u16,
+}
+
 fn main() {
     println!("Hello, world!");
 }
