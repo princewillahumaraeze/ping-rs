5274340f6f5a7749d46da999043f3f699935f038
Ahumaraeze Princewill <princewillahumaraeze@gmail.com>
2025-06-01 20:33:14 +0100

    added implementations for the Icmp packet struct

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,44 @@ struct IcmpEcho {
     payload: Vec<u8>,
 }
 
+impl IcmpEcho{
+    fn new(identifier: u16, sequence_number:u16, payload: Vec<u8>) -> Self{
+        IcmpEcho { 
+            icmp_type: 8,
+            icmp_code: 0,
+            checksum: 0,
+            identifier,
+            sequence_number,
+            payload,
+        }
+    }
+
+    fn to_bytes(&self) -> Vec<u8> {
+        let packet_payload_len = 8 + self.payload.len();
+        let mut packet = Vec::with_capacity(packet_payload_len);
+
+        // Icmp Header
+        packet.push(self.icmp_type);
+        packet.push(self.icmp_code);
+        packet.push(0); //Checksum placeholder
+        packet.push(0);
+        packet.push((self.identifier >> 8) as u8);
+        packet.push((self.identifier & 0xFF) as u8);
+        packet.push((self.sequence_number >> 8) as u8);
+        packet.push((self.sequence_number & 0xFF) as u8);
+
+        // Payload
+        packet.extend_from_slice(&self.payload);
+
+        // Calculate checksum over entire packet
+        let cs = checksum(&packet);
+        packet[2] = (cs >> 8) as u8;
+        packet[3] = (cs & 0xFF) as u8;
+        
+        packet
+    }
+}
+
 
 fn checksum(data: &[u8]) -> u16 {
     let mut sum =  0u32;
